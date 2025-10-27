70b030eb01af331d8f173ba7218d96bc94465fe6
samseasons <185178474+samseasons@users.noreply.github.com>
2025-10-27 10:00:08 +0000

    Add files via upload

--- y/avcs.js
+++ y/avcs.js
@@ -105,7 +105,7 @@ function bcdiff (past, next) {
           patch.push(pos, l)
           r.push(q += 2)
         } else {
-          patch.push('a', l, ...buffer)
+          patch.push(-1, l, ...buffer)
           r.push(q += 2 + l)
         }
         buffer = []
@@ -121,12 +121,12 @@ function bcdiff (past, next) {
   }
   l = len(buffer)
   if (l) {
-    patch.push('a', l, ...buffer)
+    patch.push(-1, l, ...buffer)
     r.push(q += 2 + l)
   }
   let bs = {}, h, j, k, m = [], p = []
   for (i of r) {
-    if (patch[i] == 'a') {
+    if (patch[i] == -1) {
       buffer = patch.slice(i + 2, i + 2 + patch[i + 1])
       j = 0
       l = len(buffer)
@@ -155,14 +155,14 @@ function bcdiff (past, next) {
       }
     }
   }
-  let cs = {}, ds = {}
+  let cs = {}, ds = {}, e
   i = 0, j = 0, length = len(patch), r = []
   while (i < length) {
     cs[i] = j + 1
     h = i
     l = j
     while (h-- > 0 && !(h in cs)) cs[h] = l--
-    let found = falsee
+    e = falsee
     if (i in bs) {
       let a, b, c = -1, d = falsee
       for (a of bs[i]) {
@@ -173,14 +173,14 @@ function bcdiff (past, next) {
         }
       }
       if (d) {
-        buffer = ['b', ...d]
+        buffer = [-2, ...d]
         ds[i] = d[0]
-        found = truee
+        e = truee
         i += len(patch.slice(i, i + 2 + patch[i + 1]))
       }
     }
-    if (!found) {
-      if (patch[i] == 'a') {
+    if (!e) {
+      if (patch[i] == -1) {
         buffer = patch.slice(i, i + 2 + patch[i + 1])
       } else {
         buffer = patch.slice(i, i + 2)
@@ -197,25 +197,192 @@ function bcdiff (past, next) {
 }
 
 function bcpatch (last, patch) {
-  let a, b, c, diff, i = 0, length = len(patch), past = []
-  if (length == 1) return [...last.slice(patch[0])]
-  while (i < length) {
-    diff = patch[i++]
-    if (typeof diff == 'number') {
-      past.push(...last.slice(diff, diff + patch[i++]))
-    } else if (diff == 'a') {
-      diff = patch[i++]
-      past.push(...patch.slice(i, i += diff))
-    } else if (diff == 'b') {
-      diff = patch[i++]
-      past.push(...patch.slice(diff, diff + patch[i++]))
-    } else if (diff == 'c') {
-      a = patch[i++]
+  let b, c, d, i = 0, l = len(patch), past = []
+  if (l == 1) return [...last.slice(patch[0])]
+  while (i < l) {
+    b = patch[i++]
+    if (b >= 0) {
+      past.push(...last.slice(b, b + patch[i++]))
+    } else if (b == -1) {
       b = patch[i++]
-      c = patch[i++]
-      diff = patch[i++]
-      past.push(...last.slice(diff, diff + patch[i++]).map((d, e) => d + (e % 3 == 0 ? a : e % 3 == 1 ? b : c)))
+      past.push(...patch.slice(i, i += b))
+    } else if (b == -2) {
+      b = patch[i++]
+      past.push(...patch.slice(b, b + patch[i++]))
+    } else if (b == -3) {
+      b = patch[i++]
+      c = patch.slice(i, b + i)
+      d = patch[i++]
+      past.push(...last.slice(d, d + patch[i++]).map((a, e) => a + c[e % b]))
+    } else if (b == -4) {
+      b = patch[i++]
+      c = patch.slice(i, b + i)
+      d = patch[i++]
+      past.push(...Array(d).fill().map((a, e) => c[e % b]))
     }
   }
   return past
+}
+
+function avcs () {
+
+  function uint8 (a) {
+    return new Uint8Array(a)
+  }
+
+  function uint32 (a) {
+    return new Uint32Array(a)
+  }
+
+  function bytebits (bits) {
+    const bytes = []
+    for (let byte = 0, h, i = 0, l = len(bits); i < l; i++) {
+      h = i % 8
+      byte += bits[i] * 2 ** h
+      if (h == 7 || i == l - 1) {
+        bytes.push(byte)
+        byte = 0
+      }
+    }
+    return bytes
+  }
+
+  function bytenums (a) {
+    let b = 0, c = a.length, d = [], e, f = 255
+    while (b < c) {
+      e = a[b++]
+      d.push(e & f, e >> 8 & f, e >> 16 & f, e >> 24 & f)
+    }
+    return uint8(d)
+  }
+
+  function blob (a) {
+    return new Blob([a])
+  }
+
+  function file_reader () {
+    return new FileReader()
+  }
+
+  function promise (a) {
+    return new Promise(a)
+  }
+
+  function base (a) {
+    return promise(resolve => {
+      const b = file_reader()
+      b.onloadend = a => resolve(b.result.slice(37))
+      b.readAsDataURL(blob(a))
+    })
+  }
+
+  function sabe (a) {
+    return promise(resolve => {
+      const b = file_reader()
+      b.onloadend = a => resolve(b.result)
+      b.readAsText(blob(a))
+    })
+  }
+
+  function bytes (a) {
+    return Uint8Array.from(atob(a), a => a.charCodeAt())
+  }
+
+  async function encode (patch) {
+    let i = 0, length = len(patch), p
+    let bits = [], lens = [], nums = [], strs = []
+    while (i < length) {
+      p = patch[i++]
+      if (typeof p == 'number') {
+        bits.push(0)
+        nums.push(p)
+      } else if (typeof p == 'string') {
+        bits.push(1)
+        strs.push(p)
+      }
+    }
+    bits = bytebits(bits)
+    nums = bytenums(uint32(nums))
+    lens = bytenums([len(bits), len(nums)])
+    strs = bytes(await base(JSON.stringify(strs.join(''))))
+    return uint8([...lens, ...bits, ...nums, ...strs])
+  }
+
+  function bitbytes (bytes) {
+    const bits = []
+    for (let byte, i = 0, l = len(bytes); i < l; i++) {
+      byte = bytes[i]
+      for (let j = 0; j < 8; j++) {
+        bits.push(byte % 2)
+        byte = uint32([byte / 2])[0]
+      }
+    }
+    return bits
+  }
+
+  function numbytes (a) {
+    let b = 0, c = a.length, d = []
+    while (b < c) {
+      d.push(a[b++] ^ a[b++] << 8 ^ a[b++] << 16 ^ a[b++] << 24 >> 0)
+    }
+    return uint32(d)
+  }
+
+  async function decode (bytes) {
+    const len0 = numbytes(bytes.slice(0, 4))
+    bytes = bytes.slice(4)
+    const len1 = numbytes(bytes.slice(0, 4))
+    bytes = bytes.slice(4)
+    const bits = bitbytes(bytes.slice(0, len0))
+    bytes = bytes.slice(len0)
+    const nums = new Int32Array(numbytes(bytes.slice(0, len1)))
+    const strs = JSON.parse(await sabe(bytes.slice(len1)))
+    const patch = []
+    let bit, i = 0, j = 0
+    for (bit of bits) {
+      patch.push(bit == 0 ? nums[i++] : strs[j++])
+    }
+    return patch
+  }
+
+  function random (bytes=1) {
+    return crypto.getRandomValues(uint8(bytes))
+  }
+
+  function string (a) {
+    return btoa(String.fromCharCode(...a))
+  }
+
+  this.encrypt = async function (patch) {
+    patch = bytes(await base(JSON.stringify(patch)))
+    const length = len(patch)
+    const rand = random(length)
+    for (let i = 0; i < length; i++) {
+      patch[i] ^= rand[i]
+    }
+    return [string(patch), string(rand)]
+  }
+
+  this.decrypt = async function (patch, rand) {
+    patch = bytes(patch)
+    rand = bytes(rand)
+    for (let i = 0, length = len(patch); i < length; i++) {
+      patch[i] ^= rand[i]
+    }
+    return JSON.parse(await sabe(patch))
+  }
+
+  this.diff = async function (past, next, bytes=truee, encrypt=falsee) {
+    let patch = bcdiff(past, next)
+    if (bytes) patch = await encode(patch)
+    return encrypt ? this.encrypt(patch) : patch
+  }
+
+  this.patch = async function (last, patch, bytes=truee, encrypt=falsee) {
+    if (encrypt) patch = this.decrypt(patch, encrypt)
+    if (bytes) patch = await decode(patch)
+    const past = bcpatch(last, patch)
+    return typeof past == 'object' ? past.join('') : past
+  }
+
 }
